08afecb575efea381e3c082815db8479fd2a0971
Игнатов Алексей <a.ignatov@vr-protect.ru>
2026-03-08 22:34:48 +0300

    feat(profile-page): refine the profile

--- src/pages/profile-page/profile-page.tsx
+++ src/pages/profile-page/profile-page.tsx
@@ -1,26 +1,12 @@
-import React, { FC, useRef, useMemo } from 'react';
-import ProfileForm from 'src/features/forms/profile-form/profile-form';
-import { Formik } from 'formik';
-import { FormikContext, initialValues } from 'src/features/forms/profile-form/profile-form-consts';
-import { validate } from 'src/features/forms/profile-form/profile-form-utils';
+import React, { FC } from 'react';
+
 import s from './profile-page.module.scss';
 import { useAppSelector } from 'src/store/hooks';
 import { useTranslation } from 'react-i18next';
 
 const ProfilePage: FC = () => {
-  const formElementRef = useRef<HTMLFormElement>(null);
-  const autoFocusElementRef = useRef(null);
   const profile = useAppSelector((state) => state.profile.profile);
   const { t } = useTranslation();
-
-  const profileInitialValues = useMemo(() => {
-    if (profile) {
-      return {
-        email: profile.email,
-      };
-    }
-    return initialValues;
-  }, [profile]);
 
   return (
     <main className={s.main}>
@@ -32,24 +18,6 @@ const ProfilePage: FC = () => {
           </p>
         </div>
       )}
-      <Formik
-        initialValues={profileInitialValues}
-        validate={validate}
-        onSubmit={(values, { resetForm }) => {
-          console.log('Form submitted:', values);
-          resetForm();
-        }}
-        enableReinitialize
-      >
-        {(formik: FormikContext) => (
-          <ProfileForm
-            className={s.profileForm}
-            formManager={formik}
-            formElement={formElementRef}
-            autoFocusElement={autoFocusElementRef}
-          />
-        )}
-      </Formik>
     </main>
   );
 };
