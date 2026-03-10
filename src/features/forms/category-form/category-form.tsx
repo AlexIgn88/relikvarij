51563fc41294228393657296e890a468a6ad56be
Игнатов Алексей <a.ignatov@vr-protect.ru>
2026-03-10 15:13:57 +0300

    style(form.module): change styles in category-form

--- src/features/forms/category-form/category-form.tsx
+++ src/features/forms/category-form/category-form.tsx
@@ -9,51 +9,48 @@ import PhotoField from 'src/features/forms/product-operation-form/photo-field/ph
 import formStyle from 'src/features/forms/form.module.scss';
 import s from './category-form.module.scss';
 
-const CategoryForm = memo<CategoryFormProps>(
-  ({ className, formManager, formElement, autoFocusElement, disabled }) => {
-    const { t } = useTranslation();
-    const { values, touched, errors, submitCount, handleBlur, handleSubmit, handleChange } = formManager;
+const CategoryForm = memo<CategoryFormProps>(({ className, formManager, formElement, autoFocusElement, disabled }) => {
+  const { t } = useTranslation();
+  const { values, touched, errors, submitCount, handleBlur, handleSubmit, handleChange } = formManager;
 
-    return (
-      <form ref={formElement} onSubmit={handleSubmit} className={cn(s.root, className)}>
-        <NameField
-          autoFocusElement={autoFocusElement}
-          onBlur={handleBlur}
-          onChange={handleChange}
-          value={values.name}
-          errors={errors.name || ''}
-          submitCount={submitCount}
-          touched={touched.name || false}
-          disabled={disabled}
-          label={t('forms.CategoryForm.name.title')}
-          placeholder={t('forms.CategoryForm.name.placeholder')}
-        />
+  return (
+    <form ref={formElement} onSubmit={handleSubmit} className={cn(s.root, className)}>
+      <NameField
+        autoFocusElement={autoFocusElement}
+        onBlur={handleBlur}
+        onChange={handleChange}
+        value={values.name}
+        errors={errors.name || ''}
+        submitCount={submitCount}
+        touched={touched.name || false}
+        disabled={disabled}
+        label={t('forms.CategoryForm.name.title')}
+        placeholder={t('forms.CategoryForm.name.placeholder')}
+      />
 
-        <PhotoField
-          onBlur={handleBlur}
-          onChange={handleChange}
-          value={values.photo || ''}
-          errors={errors.photo || ''}
-          submitCount={submitCount}
-          touched={touched.photo || false}
-          disabled={disabled}
-          label={t('forms.CategoryForm.photo.title')}
-          placeholder={t('forms.CategoryForm.photo.placeholder')}
-        />
+      <PhotoField
+        onBlur={handleBlur}
+        onChange={handleChange}
+        value={values.photo || ''}
+        errors={errors.photo || ''}
+        submitCount={submitCount}
+        touched={touched.photo || false}
+        disabled={disabled}
+        label={t('forms.CategoryForm.photo.title')}
+        placeholder={t('forms.CategoryForm.photo.placeholder')}
+      />
 
-        <div className={formStyle.buttonContainer}>
-          <div className={formStyle.buttonWrapper}>
-            <Button type="primary" htmlType="submit" disabled={disabled} block>
-              {t('forms.CategoryForm.submit')}
-            </Button>
-          </div>
+      <div className={formStyle.buttonContainer}>
+        <div className={formStyle.buttonWrapper}>
+          <Button type="primary" htmlType="submit" disabled={disabled} block>
+            {t('forms.CategoryForm.submit')}
+          </Button>
         </div>
-      </form>
-    );
-  }
-);
+      </div>
+    </form>
+  );
+});
 
 CategoryForm.displayName = 'CategoryForm';
 
 export default CategoryForm;
-
